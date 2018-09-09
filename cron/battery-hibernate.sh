362cca085fbcffdb399e25503213d90ec603c447
aksel <aksel@akseltorgard.com>
2018-09-09 20:46:40 +0200

    Added script, hibernates on low battery

---
+++ cron/battery-hibernate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+status=$(cat /sys/class/power_supply/BAT0/status)
+
+if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
+    logger "Critical battery threshold"
+    i3lock -c 000000
+    systemctl hibernate
+fi
