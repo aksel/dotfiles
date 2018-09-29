a5cac65e21a18faf5b8489f2ba4d18d10034621d
aksel <aksel@akseltorgard.com>
2018-09-29 19:33:56 +0200

    Fixed battery hibernate script; must use sudo for systemctl from cron. i3lock still does not work

--- cron/battery-hibernate.sh
+++ cron/battery-hibernate.sh
@@ -5,6 +5,6 @@ status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
     logger "Critical battery threshold"
-    i3lock -c 000000
-    systemctl hibernate
+    #i3lock -c 000000
+    sudo systemctl hibernate
 fi
