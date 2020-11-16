1928ddd4a6658fe5986578282b775d8b61364c4d
aksel <aksel@akseltorgard.com>
2020-11-16 12:15:18 +0100

    Updated backlight device.

--- .config/i3blocks/brightness.sh
+++ .config/i3blocks/brightness.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-current=$(cat /sys/class/backlight/intel_backlight/brightness)
+max=$(cat /sys/class/backlight/amdgpu_bl0/max_brightness)
+current=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
 
 brightness=$((100 * $current / $max))
 
