671fea9bfea11a69eb0cc4da98bbc7a1ea088002
aksel <aksel@akseltorgard.com>
2020-11-21 22:00:20 +0100

    Updated i3blocks
    Name all blocklets without suffix; simplifies config
    blocklet output: use printf, and add space padding.
    Added custom cpu_usage blocklet
    Added urgency to memory blocklet
    Reimplemented volume blocklet

--- .config/i3blocks/memory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-total=$(vmstat -s | grep "total memory" | perl -pe 's/\D//g')
-used=$(vmstat -s | grep "used memory" | perl -pe 's/\D//g')
-
-echo "$total $used" | awk '{printf "%.2f%", $2/$1*100}' 
-
